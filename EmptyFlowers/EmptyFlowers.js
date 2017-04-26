d1e4116d7578ba6889cc28ab75be23c7f9eb55b2
Charlie <cafornaca@yahoo.com>
2017-04-25 21:42:22 -0700

    Added EmptyFlowers

---
+++ EmptyFlowers/EmptyFlowers.js
@@ -0,0 +1,20 @@
+    $(document.body).click(function() {
+
+        var images = ['slide_1', 'slide_2', 'slide_3', 'slide_4', 'slide_5', 'slide_6', 'slide_7', 'slide_8', 'slide_9', 'slide_10'];
+        var quotes = [
+            "Nothing matters. Everything ends.",
+            "I can't go on, I'll go on.",  
+            "The point is there ain't no point.",
+            "What's disappointing is that it's just another mold, you leave one cliche and end up right back in another. Not really that subversive or out of control.",
+            "If we believe in nothing, if nothing has any meaning and if we can affirm no values whatsoever, then everything is possible and nothing has any importance.",
+            "There is no other world. Nor even this one. What, then, is there? The inner smile provoked in us by the patent nonexistence of both.",
+            "Every existing thing is born without reason, prolongs itself out of weakness and dies by chance.",
+            "It is intoxicating joy for the sufferer to look away from his suffering and to forget himself.",
+            "Maybe we really are made of the same clay, maybe we really are condemned, blameless, to the same, identical mediocrity.",
+            "If all we seek is an escape, what does that say about the world we live in. We are desperate with our dreams. What - oh, what - does that say?",
+            // "",
+            // "There's not a drug on earth can make life meaningful",
+            // "If nothing had any meaning, you would be right. But there is something that still has a meaning.",
+          ]
+    }
+  
---
+++ EmptyFlowers/README.md
@@ -0,0 +1,2 @@
+# EmptyFlowers
+Flowers will always brighten someone's day!
