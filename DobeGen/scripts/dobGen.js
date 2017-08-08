dd772907676827b0b460ab0ab3a78c1599088945
Charlie Ann <Charlie.A.Fornaa@kp.org>
2017-08-08 11:52:41 -0700

    Made many changes
    I touched DobeGen again! Gosh, my coding skills and style have improved
    so much. FINALLY got the entire background to work on CircadianScreen.

--- DobeGen/scripts/dobGen.js
+++ DobeGen/scripts/dobGen.js
@@ -1,98 +1,84 @@
+// Charlie's Doberman Generator -- June 2015
+
 $(function() {
+  // Global variables
+  var tempProb;
+  // Traits that are not associated with coat color:
+  var sex, line, ears, tail;
 
-//Charlie's Doberman Generator -- June 2015
-
-
-////global variables
-var tempProb;
-
-
-////traits not associated with coat color
-var sex;
-var line;
-var ears;
-var tail;
-
-//sex
-if (Math.random() < 0.5) {
+  // Define the sex of the generated doberman.
+  if (Math.random() < 0.5) {
     sex = "Her";
-} else {
+  } else {
     sex = "His";
-}
+  }
 
-//line
-if (Math.random() < 0.5) {
+  // Line influences what the body of the doberman will look like.
+  if (Math.random() < 0.5) {
     line = "an American";
-} else {
+  } else {
     line = "a European";
-}
+  }
 
-//ears
-tempProb = Math.random();
-if (tempProb < 0.25) {
+  // Generates what the ears of the doberman will look like.
+  tempProb = Math.random();
+  if (tempProb < 0.25) {
     ears = "uncropped/floppy";
-} else if (tempProb < 0.5) {
+  } else if (tempProb < 0.5) {
     ears = "cropped show-style";
-} else if (tempProb < 0.75) {
+  } else if (tempProb < 0.75) {
     ears = "cropped military-style";
-} else {
+  } else {
     ears = "cropped pet-style";
-}
+  }
 
-//tail
-tempProb = Math.random();
-if (tempProb < 0.33) {
+  // Generates the appearance of the tail.
+  tempProb = Math.random();
+  if (tempProb < 0.33) {
     tail = "uncropped/long";
-} else if (tempProb < 0.66) {
+  } else if (tempProb < 0.66) {
     tail = "docked show-style";
-} else {
+  } else {
     tail = "docked military/pet-style";
-}
+  }
 
 
-////traits associated with coat color
-var coatColorSimple;
-var nose;
-var eyes;
+  // Variabls for traits associated with coat color
+  var coatColorSimple, nose, eyes;
 
-tempProb = Math.random();
-if (tempProb < 0.2) {
+  tempProb = Math.random();
+  if (tempProb < 0.2) {
     coatColorSimple = "black with tan markings";
     nose = "black";
     eyes = "brown";
 
-} else if (tempProb < 0.4) {
+  } else if (tempProb < 0.4) {
     coatColorSimple = "blue-gray with brown markings";
     nose = "dark gray";
     eyes = "gray";
 
-
-} else if (tempProb < 0.6) {
+  } else if (tempProb < 0.6) {
     coatColorSimple = "red with brown markings";
     nose = "brown";
     eyes = "amber";
 
-
-
-} else if (tempProb < 0.8) {
+  } else if (tempProb < 0.8) {
     coatColorSimple = "Isabella/fawn colored with rust markings";
     nose = "liver-colored";
     eyes = "amber";
 
-
-
-} else {
+  } else {
     coatColorSimple = "white with cream markings";
     nose = "pink";
     eyes = "light blue";
+  }
 
-}
+  // Print statement to print it all:
+  $(".print").append("You doberman is of " + line + " line. " + sex + " ears are " + ears + ". " + sex + " tail is " + tail + ". Your doberman is " + coatColorSimple + ". " + sex + " eyes are " + eyes + ". " + sex + " nose and paw pads are " + nose + ".");
+  });
 
 
-////print
-$(".print").append("You doberman is of " + line + " line. " + sex + " ears are " + ears + ". " + sex + " tail is " + tail + ". Your doberman is " + coatColorSimple + ". " + sex + " eyes are " + eyes + ". " + sex + " nose and paw pads are " + nose + ".");
-
-});
-
-////credits
-//http://orig09.deviantart.net/8560/f/2013/293/c/a/doberman_pinscher_coat_color_and_anatomy_guide_by_xlunastarx-d6r7m4r.png
+/*
+ * Credits:
+ * http://orig09.deviantart.net/8560/f/2013/293/c/a/doberman_pinscher_coat_color_and_anatomy_guide_by_xlunastarx-d6r7m4r.png
+*/
